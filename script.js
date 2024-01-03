a018e86332f36fa47cdc55f08fb3597799ec1f9f
Hilary Jannay Mora Ruiz <hilarymoraruiz@.com>
2024-01-03 17:52:38 -0600

    Sidebar cart added

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function recargarPagina() {
     location.reload(); // Esta línea recarga la página
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Pagina cargada totalmente!")
+
+    const shoppingCartButton = document.querySelector(".shop_img");
+    const shoppingCart = document.querySelector(".sidebar_cart");
+
+    shoppingCartButton.addEventListener("click", () => {
+        shoppingCart.classList.toggle("open");
+    })
+})
+
+
